8a99fdb502a84454190bd49facf9adf7a8ec5f73
Leonardo <cifuentes.k.l@gmail.com>
2020-11-19 20:31:43 -0300

    wip/Navbar_component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Cards from './components/Cards';
+import Character from './components/Character';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Route path="/" render={() => <Navbar />} />
+      {/* <Route exact path="/" render={() => <Home />} />
+      <Route exact path="/search" render={() => <Cards />} />
+      <Route exact path="/character/:id" render={() => <Character />} />
+      <Route path="/" render={() => <Footer />} /> */}
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -12,9 +11,7 @@ import { store } from './redux/storeCreator';
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-      <Router>
         <App />
-      </Router>
     </React.StrictMode>
   </Provider>,
   document.getElementById('root')
