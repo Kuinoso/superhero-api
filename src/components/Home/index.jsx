380117204a33cddeb6af8220ec106e97303e494b
Leonardo <cifuentes.k.l@gmail.com>
2020-11-23 19:54:32 -0300

    wip/api_bug?

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Paper from '@material-ui/core/Paper';
 import SearchIcon from '@material-ui/icons/Search';
 import InputBase from '@material-ui/core/InputBase';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { useStyles } from './styles.js';
+import axios from 'axios';
 
 export default function Home() {
+    const [search, setSearch] = useState(null)
     const classes = useStyles();
+
+    const handleInput = (e) => {
+        setSearch(e.target.value)
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await axios.get(`https://superheroapi.com/api/10157699190431317/10`, {
+            headers: {
+                "Content-type": "application/json",
+                "Access-Control-Allow-Methods": "GET,OPTIONS,POST",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers":
+                    "Access-Control-Allow-Headers,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Authorization,Content-Type",
+            }
+        })
+        console.log(response)
+    }
 
     return (
         <div className={classes.container}>
@@ -19,8 +39,9 @@ export default function Home() {
                     className={classes.input}
                     placeholder="Search hero, villain, etc..."
                     inputProps={{ 'aria-label': 'search google maps' }}
+                    onChange={handleInput}
                 />
-                <IconButton type="submit" className={classes.iconButton} aria-label="search">
+                <IconButton type="submit" className={classes.iconButton} aria-label="search" onClick={handleSubmit}>
                     <SearchIcon />
                 </IconButton>
             </Paper>
